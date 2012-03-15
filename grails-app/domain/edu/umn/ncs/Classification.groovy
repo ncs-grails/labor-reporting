a6c59cd74fe87f3677aad028ee32210604dc2d3d
Simone Q. Vuong <sqv@umn.edu>
2012-03-15 19:08:27 +0000

    Add properties to domain class Classification. Removed target/ folder
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/labor-reporting@971 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/domain/edu/umn/ncs/Classification.groovy
+++ grails-app/domain/edu/umn/ncs/Classification.groovy
@@ -1,7 +1,25 @@
 package edu.umn.ncs
+import org.codehaus.groovy.grails.plugins.orm.auditable.AuditLogEvent
 
 class Classification {
 
+    static auditable = true
+	
+	String name
+	Boolean active = false
+	Date dateCreated = new Date()
+	String userCreated
+	String appCreated = 'labor-reporting'
+
+	String toString() { name }	
+
     static constraints = {
+		name(blank:false, maxSize:96)
+		active
+		dateCreated
+		userCreated(blank:false)
+		appCreated(blank:false)
     }
+
+
 }
